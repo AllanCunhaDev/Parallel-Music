52e7e2003429c2bb666ae473c9285d8b197bb0f8
Allan <allancunha20@hotmail.com>
2025-02-19 17:24:00 -0300

    Browser routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,26 +3,34 @@ import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 import ItemList from "./ItemList";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
       {/* ItensList de Artitas */}
-      <ItemList
-        title="Artistas"
-        items={5}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"
-      />
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={5}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
 
       {/* ItensList de Musicas */}
-      <ItemList
-        title="Musicas"
-        items={10}
-        itemsArray={songsArray}
-        path="/songs"
-        idPath="/song"
-      />
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Musicas"
+          items={10}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
